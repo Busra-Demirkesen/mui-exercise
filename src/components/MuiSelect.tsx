e79dab4ae9627038e07f3c7aa51f1f4c9d31a600
Busra-Demirkesen <busrademirkesen2@gmail.com>
2025-05-27 15:33:24 +0200

    refactor: clean up code formatting and improve readability in MuiSelect component

--- src/components/MuiSelect.tsx
+++ src/components/MuiSelect.tsx
@@ -5,14 +5,12 @@ const MuiSelect = () => {
 
   console.log(countries);
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-  const value = event.target.value;
-  setCountries(
-    typeof value === 'string' ? value.split(',') : value
-  );
-};
+    const value = event.target.value;
+    setCountries(typeof value === "string" ? value.split(",") : value);
+  };
 
   return (
-    <Box width={'250px'}>
+    <Box width={"250px"}>
       <TextField
         label="select country"
         select
@@ -20,8 +18,11 @@ const MuiSelect = () => {
         onChange={handleChange}
         fullWidth // with 100% gibi düşünülebilir
         SelectProps={{
-            multiple:true, // birden fazla seceneği secebilmek için kullanıyoruz
+          multiple: true, // birden fazla seceneği secebilmek için kullanıyoruz
         }}
+        size='small'
+        color='secondary'
+        helperText='please select your country'
       >
         <MenuItem value="IN">India</MenuItem>
         <MenuItem value="US">USA</MenuItem>
